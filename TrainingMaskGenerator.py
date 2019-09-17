a985ecb26a2a9e68358533185303bf4974983070
kristinagessel <kmgessel@gmail.com>
2019-09-16 20:46:20 -0400

    First try at setting a radius for each seed point

--- TrainingMaskGenerator.py
+++ TrainingMaskGenerator.py
@@ -24,6 +24,10 @@ class TrainingMaskGenerator:
         output = reader.read(self.path_to_high_res_json, page_num, "_output.txt")
         filtered_output = self.filter_output(output)
         pg_seg_pixels = self.do_floodfill(filtered_output, page_num)
+        #TODO: later use the pg_seg_pixels returned by do_floodfill to stitch all the pages together in one slice image?
+
+        #TODO: eventually... to do instance segmentation we need polygons, not just points. When we get to that point, make the polygons with ONLY directly connected pixels. There are some weird straggler points not connected
+        #to the main group of points
 
     def filter_output(self, output):
         filtered_output = {}
@@ -47,10 +51,13 @@ class TrainingMaskGenerator:
 
         for slice in filtered_output:
             slice_num = slice.zfill(4)
-            im = cv2.imread(self.img_path + slice_num + ".tif")
-            stack = filtered_output[slice].copy()
-            start_pts = filtered_output[slice].copy()
+            stack = []
             visited = []
+            im = cv2.imread(self.img_path + slice_num + ".tif")
+            for elem in filtered_output[slice]:
+                stack.append((elem, elem)) #append a tuple of ((point), (origin point)) to keep track of how far we are from the original point
+            #stack = filtered_output[slice].copy()
+            start_pts = filtered_output[slice].copy()
             height, width, channels = im.shape
 
             avg = self.calc_avg_pg_width(start_pts, im) #avg width might change some in different slices?
@@ -58,13 +65,13 @@ class TrainingMaskGenerator:
             while stack: #"while stack is not empty"
                 point = stack.pop()
                 visited.append(point)
-                x = int(point[0])
-                y = int(point[1])
+                x = int(point[0][0]) # point of interest's x (not origin point's x)
+                y = int(point[0][1])
                 im[y][x] = (255, 0, 0) #make the visited point blue
-                valid_neighbors= self.floodfill_check_neighbors(im, point, height, width, avg, start_pts)
+                valid_neighbors= self.floodfill_check_neighbors(im, point, height, width, avg)
                 for pt in valid_neighbors:
                     if pt not in visited and pt not in stack:
-                        stack.append(pt)
+                        stack.append((pt, point[1])) #append a tuple of form ((point), (origin point for parent point))
 
                 iter += 1
                 if iter % 500 == 0:
@@ -82,11 +89,6 @@ class TrainingMaskGenerator:
         return seg_pixels
 
 
-    #TODO: could set a limit for how far the flood fill can reach out to prevent flood fill from crossing into a connected neighboring page?
-    #TODO: *****calculate the AVERAGE width of a single page by checking the width in a consistent way from each point.
-    # (thicknesses vary between pages because it's vellum, but the same page is generally the same thickness except where tears are)
-    # That way if some are extra long, we can throw them out and then give the flood fill a limit of how much it can expand by the average
-    # Maybe even calculate the distance between each point so we can set a sort of oval boundary
     #store the 'distance' in the stack too?
     def calc_avg_pg_width(self, pts, im):
         pt_counts = []
@@ -114,21 +116,33 @@ class TrainingMaskGenerator:
 
 
 
-    #TODO: use avg width to limit how far we are checking for neighbors so we don't fill more pages where pages overlap
-    #TODO: compare avg to start_pts? if current x > nearest start_pt + avg, stop? (buggy I think)
-    def floodfill_check_neighbors(self, im, pixel, height, width, avg_width, start_pts):
+    #TODO: could set a limit for how far the flood fill can reach out to prevent flood fill from crossing into a connected neighboring page?
+    #TODO: *****calculate the AVERAGE width of a single page by checking the width in a consistent way from each point.
+    # (thicknesses vary between pages because it's vellum, but the same page is generally the same thickness except where tears are)
+    # That way if some are extra long, we can throw them out and then give the flood fill a limit of how much it can expand by the average
+    # Maybe even calculate the distance between each point so we can set a sort of oval boundary
+    #ANOTHER TODO: we have some random single pixels that are getting colored... Why??? That's not flood fill...
+    def floodfill_check_neighbors(self, im, pixel, height, width, avg_width):
         valid_neighbors = []
         x = [-1, 0, 1]
         y = [-1, 0, 1]
-        x_pos = pixel[0]
-        y_pos = pixel[1]
+        x_pos = pixel[0][0]
+        y_pos = pixel[0][1]
         for i in y: #height
             for j in x: #width
-                if (y_pos != 0 or x_pos != 0) and x_pos + j < width-1 and y_pos + i < height-1: #Don't want the center pixel or any out of bounds
+                #TODO: verify that the point of interest is not further away from its origin point by more than avg_width
+                if (y_pos != 0 or x_pos != 0) and x_pos + j < width-1 and y_pos + i < height-1 and self.calculate_distance_from_origin(pixel[0], pixel[1]) <= avg_width: #Don't want the center pixel or any out of bounds
                     grey_val = im[y_pos + i][x_pos + j][0] #pixel access is BACKWARDS--(y,x)
                     if grey_val > self.low_tolerance and grey_val < self.high_tolerance:
                         valid_neighbors.append((x_pos + j, y_pos + i))
         return valid_neighbors
+
+
+    #what distance metric?
+    #for now, try just distance in the x direction?
+    #TODO: maybe later make a 'rectangle' of eligible space where x bound is average width and y bound is the y coord of the next point...?
+    def calculate_distance_from_origin(self, point, origin):
+        return abs(point[0] - origin[0])
 
 def main():
     gen = TrainingMaskGenerator()
