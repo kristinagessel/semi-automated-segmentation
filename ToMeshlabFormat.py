ed91ec8cd93642d66b37534cb026874c5ce3131c
Kristina Gessel <kmge224@uky.edu>
2019-11-03 11:19:57 -0500

    Generate 3D point cloud in Meshlab-compatible format using the JSON output for each page.

---
+++ ToMeshlabFormat.py
@@ -0,0 +1,53 @@
+import ujson
+import glob
+import os
+import cv2
+
+path_to_json_files = "/Volumes/Research/1. Research/Experiments/TrainingMasksAll/"
+path_to_dense_files = "/Volumes/Research/1. Research/Experiments/meshlab/floodfill/"
+path_to_raw_files = "/Volumes/Research/1. Research/Experiments/meshlab/vc_seg/"
+path_to_raw_pointset_files = "/Volumes/Research/1. Research/Experiments/high_res_output/"
+image_path = "/Volumes/Research/1. Research/MS910.volpkg/volumes/20180122092342/"
+
+HI_RES_PATH = "/Volumes/Research/1. Research/MS910.volpkg/volumes/20180122092342/"
+
+'''
+Read JSON
+Convert to a format readable by Meshlab, like (X Y Z ; X Y Z ; ...)
+We have X and Y, Z increments by 1 each slice.
+'''
+def convertJSONToXYZ(path, out_path):
+    files = glob.glob(path + "*.txt")
+
+    for file in files:
+        pg_file = os.path.basename(file)
+        page = pg_file[:pg_file.find(".")]
+        f = open(file)
+        output = ujson.loads(f.read())  # get the dictionary
+        f.close()
+
+        out_file = open(out_path + page + "_pointset.txt", "w")
+        z = 0
+
+        for slice in output:
+            slice_num = slice.zfill(4)
+            im = cv2.imread(image_path + slice_num + ".tif")
+            for pt in output[slice]:
+                #we get a tuple (x, y)
+                x = int(pt[0])
+                y = int(pt[1])
+                intensity = im[y][x]
+                if intensity[0] + 30 > 255:
+                    intensity[0] = 255
+                    intensity[1] = 255
+                    intensity[2] = 255
+                else:
+                    intensity[0] += 30
+                    intensity[1] += 30
+                    intensity[2] += 30
+                out_file.write(str(x) + " " + str(y) + " " + str(z) + " " + str(intensity[0]) + " " + str(intensity[1]) + " " + str(intensity[2]) + "\n")
+            z += 1
+        out_file.close()
+
+convertJSONToXYZ(path_to_raw_pointset_files, path_to_raw_files)
+convertJSONToXYZ(path_to_json_files, path_to_dense_files)
