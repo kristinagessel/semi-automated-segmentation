74e3d27798e4d95eaccf7cbe91fb3266b7ded270
Kristina Gessel <kmge224@uky.edu>
2020-02-09 14:49:26 -0500

    Refinement

--- HDF5ToPointCloud.py
+++ HDF5ToPointCloud.py
@@ -2,9 +2,9 @@ import h5py
 import numpy as np
 
 #path to file:
-path = "/Users/kristinagessel/Desktop/ProjectExperiments/floodfill-network/ffn-github-repo/test_h5pyfile.h5"
+path = "/Users/kristinagessel/Downloads/h5pyfile.h5"#"/Users/kristinagessel/Desktop/ProjectExperiments/floodfill-network/ffn-github-repo/test_h5pyfile.h5"
 
-out_file = open("point_cloud.txt", 'w')
+out_file = open("point_cloud_test.txt", 'w')
 f = h5py.File(path, 'r')
 key_list = list(f.keys())
 print(key_list)
@@ -15,6 +15,7 @@ print(dataset.dtype)
 
 num_color_dict = {}
 pg_colors = []
+obj_limit = 500
 
 #record anything that's not 0 through all the layers (shape[2] is hopefully z, verify)
 for z in range(0, dataset.shape[2]):
@@ -24,14 +25,16 @@ for z in range(0, dataset.shape[2]):
                 # if object class has been seen before, use the same color
                 if dataset[x][y][z] in num_color_dict.keys():
                     color = num_color_dict[dataset[x][y][z]]
-                else:
+                    out_file.write(str(x) + " " + str(y) + " " + str(z) + " " + str(color[0]) + " " + str(color[1]) + " " + str(color[2]) + "\n")
+                elif len(num_color_dict.keys()) < obj_limit:
                     color = tuple(np.random.choice(range(256), size=3))
-                    while color in pg_colors:
-                        color = tuple(np.random.choice(range(256), size=3))
-                    pg_colors.append(color)
-                    num_color_dict[dataset[x][y][z]] = color
+                    if(len(num_color_dict.keys()) < 10):
+                        while color in pg_colors:
+                            color = tuple(np.random.choice(range(256), size=3))
+                        pg_colors.append(color)
+                        num_color_dict[dataset[x][y][z]] = color
 
-                out_file.write(str(x) + " " + str(y) + " " + str(z) + " " + str(color[0]) + " " + str(color[1]) + " " + str(color[2]) +"\n")
+                    out_file.write(str(x) + " " + str(y) + " " + str(z) + " " + str(color[0]) + " " + str(color[1]) + " " + str(color[2]) +"\n")
 
 f.close()
 out_file.close()
