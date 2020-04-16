3e452edc89ca792e371176e424b3886ff1c55ddb
Kristina Gessel <kmge224@uky.edu>
2020-04-16 14:10:56 -0400

    Simple function to prune the pointsets back to the last good segmentation.

---
+++ PrunePointsets.py
@@ -0,0 +1,26 @@
+import argparse
+import glob
+import os
+
+import ujson
+
+
+def prune_pointset(path, bound):
+    pts = {}
+    new_dict = {}
+    files = glob.glob(path)
+    for file in files:
+        f = open(file)
+        temp_pts = ujson.load(f)
+        new_dict = {k:temp_pts[k] for k in temp_pts if int(k) <= int(bound) and k in temp_pts}
+    return new_dict
+
+parser = argparse.ArgumentParser(description="Perform semi-automated segmentation with flood-filling and skeletonization")
+parser.add_argument("pointset_path", type=str, help="Full path to the pointset")
+parser.add_argument("save_path", type=str, help="Path to the output directory.")
+parser.add_argument("upper_bound", type=int, help="Upper bound of the pointset to keep.")
+args = parser.parse_args()
+
+dict = prune_pointset(args.pointset_path, args.upper_bound)
+pruned_file = open(args.save_path, 'w+')
+ujson.dump(dict, pruned_file, indent=1)
