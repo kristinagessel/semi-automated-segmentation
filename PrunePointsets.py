1ae0b66ea0e42ec6ff2c90ff1b4c15102eb096f0
Kristina Gessel <kmge224@uky.edu>
2020-06-15 15:53:13 -0400

    Make my life easier by making PrunePointsets bulk prune everything in a given directory.

--- PrunePointsets.py
+++ PrunePointsets.py
@@ -1,6 +1,7 @@
 import argparse
 import glob
 import os
+import re
 
 import ujson
 
@@ -15,12 +16,33 @@ def prune_pointset(path, bound):
         new_dict = {k:temp_pts[k] for k in temp_pts if int(k) <= int(bound) and k in temp_pts}
     return new_dict
 
+def auto_prune_pointset(path):
+    pts = {}
+    new_dict = {}
+    folders = glob.glob(path)
+    for dir in os.listdir(path):
+        #get the number in "toxx?x?x?"
+        regex = re.compile(r'\d+')
+        print(dir)
+        numbers = regex.findall(dir)
+        if(not len(numbers) == 0 and dir.find(".txt") == -1):
+            try:
+                slice_num = numbers[1] #Always the second set of numbers...
+                file_path = os.path.join(os.path.join(path, dir), slice_num + ".txt")
+                pruned = prune_pointset(file_path, slice_num)
+                pruned_file = open(os.path.join(args.save_path, slice_num + ".txt"), 'w+')
+                ujson.dump(pruned, pruned_file, indent=1)
+            except:
+                print("Error with directory " + dir)
+
+
 parser = argparse.ArgumentParser(description="Prune pointsets to a specified slice number to eliminate slices with errors.")
 parser.add_argument("pointset_path", type=str, help="Full path to the pointset")
 parser.add_argument("save_path", type=str, help="Path to the output directory.")
-parser.add_argument("upper_bound", type=int, help="Upper bound of the pointset to keep.")
 args = parser.parse_args()
 
-dict = prune_pointset(args.pointset_path, args.upper_bound)
-pruned_file = open(args.save_path, 'w+')
-ujson.dump(dict, pruned_file, indent=1)
+#dict = prune_pointset(args.pointset_path, args.upper_bound)
+dict = auto_prune_pointset(args.pointset_path)
+
+
+
